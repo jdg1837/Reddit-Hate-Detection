9790c822cd020205cfe7c65e810e57e5636dad1b
Juan Diego Gonzalez <juan.digo97@gmail.com>
2020-05-11 19:04:18 -0500

    count updated

--- count_records_manually.py
+++ count_records_manually.py
@@ -3,26 +3,25 @@ import sys
 import os 
 
 if len(sys.argv) < 2:
-    print("Please include parameter file")
+    print("Please include parameter file and API file")
 param_file = sys.argv[1]
 
 subreddits = file_io.read_parameters(param_file)[1]
 src = file_io.read_parameters(param_file)[6]
-count = []
 
 subreddits = [x.lower() for x in subreddits]
 subreddits.sort()
+comment_count = {}
 
 for sub in subreddits:
-    del_count = 0
+    word_count = {}
     input_file = src + '/' + sub.lower() + '_' + src + '.json'
     data = file_io.read(input_file)['data']
+    count = len(data)
+    #print(count)
 
-    for datum in data:
-        if datum['body'] == '[removed]' in datum['body']:
-            del_count += 1
+    comment_count[sub] = count
 
-    count.append("{:15s}{:10,d}\n".format(sub,del_count))
-    
-with open(src + '/' + src + '_rem_count.txt', 'w', encoding="utf8") as f:
-    f.writelines(count)
+with open(src + '/' + src + '_count.csv', 'w', encoding="utf8") as f:
+    for sub in subreddits:
+        f.write('{},{}\n'.format(sub,comment_count[sub]))
